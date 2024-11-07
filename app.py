d225069b58f8574d024435c14d93d2cddfe097fd
christian-da-cunha <christiankaufmann87@gmail.com>
2024-11-07 20:39:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -98,21 +98,4 @@ if not fehler:
     else:
         st.success(f"Sie haben genau 10 Punkte korrekt vergeben!")
 
-# Tortengrafik der Punkteverteilung
-if vergebene_punkte == 10 and not fehler:
-    # Filtere Parteien, Punkte und Farben, um nur die mit mehr als 0 Punkten anzuzeigen
-    parteien_filtered = [partei for partei, punkte in punkte_verteilung if punkte > 0]
-    punkte_filtered = [punkte for _, punkte in punkte_verteilung if punkte > 0]
-    farben_filtered = [farben[i] for i in range(len(punkte_verteilung)) if punkte_verteilung[i][1] > 0]
 
-    # Erstellen der Tortengrafik
-    fig, ax = plt.subplots()
-    ax.pie(
-        punkte_filtered,
-        labels=parteien_filtered,
-        colors=farben_filtered,
-        autopct=lambda p: f'{int(p * sum(punkte_filtered) / 100)}' if p > 0 else '',
-        startangle=90
-    )
-    ax.axis('equal')
-    st.pyplot(fig)
